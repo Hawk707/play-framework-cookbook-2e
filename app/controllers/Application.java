c1334bd255b29191c4329fa3f0ce741348fe8526
HILTI\ALZEHAY <haythami.m.alzeini@hilti.com>
2017-07-10 14:57:28 +0800

    finished file 'play-framework-cookbook-ch022.mhtml'

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -21,4 +21,16 @@ public Result echo(String msg) {
 public Result helloRedirect() {
       return redirect(controllers.routes.Application.echo("HelloWorldv2"));
     }
+	
+private static final java.util.Map<Integer, String> productMap = new java.util.HashMap<Integer, String>();
+
+static {
+  productMap.put(1, "Keyboard");
+  productMap.put(2, "Mouse");
+  productMap.put(3, "Monitor");
+}
+
+public Result listProducts() {
+  return ok(products.render(productMap.values()));
+}
 }
