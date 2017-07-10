74f4c2cccf98a1d7d7c99aab2287fb00afe3a34a
HILTI\ALZEHAY <haythami.m.alzeini@hilti.com>
2017-07-10 14:34:31 +0800

    finished file 'play-framework-cookbook-ch020.mhtml'

---
+++ app/controllers/Application.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class Application extends Controller {
+
+public Result hello() {
+      return ok("Hello World");
+    }
+}
---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := """foo_java"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  cache,
+  javaWs,
+  "mysql" % "mysql-connector-java" % "5.1.28"
+)
+
