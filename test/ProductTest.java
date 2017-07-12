b732e84c8fa54f41e2a68eca29f5320c2585941c
HILTI\ALZEHAY <haythami.m.alzeini@hilti.com>
2017-07-12 11:15:04 +0800

    finished file 'play-framework-cookbook-ch031.mhtml'

---
+++ test/ProductTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ALZEHAY on 12/7/2017.
+ */
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProductTest {
+
+    @Test
+    public void testString() {
+        String str = "product";
+        assertEquals(7, str.length());
+    }
+}
